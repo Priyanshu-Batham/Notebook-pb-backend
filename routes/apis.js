f6eb6773b336fcdcbfa3f7192feba8209df13fdc
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-05-29 13:39:46 +0530

    salting passwords and returning jwt tokens

--- routes/apis.js
+++ routes/apis.js
@@ -1,37 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
-const userModel = require('../models/user')
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const userModel = require("../models/user");
+require('dotenv').config();
 
 //api to fetch users using GET on '/getusers'
-router.get('/getusers', async (req, res)=>{
+router.get("/getusers", async (req, res) => {
   const data = await userModel.find();
   res.send(data);
-})
-
+});
 
 //api to create a user using POST on '/createuser'
-router.post("/createuser",[
-  body('name').isLength({min: 3}),
-  body('email').isEmail(),
-  body('password').isLength({min: 5})
-],
- (req, res) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.send(errors);
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 3 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //check if request body has proper name email and password
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.send(errors);
+
+    try {
+      //salting the password before storing in database
+      const salt = await bcrypt.genSalt(10);
+      const saltedPass = await bcrypt.hash(req.body.password, salt);
+
+      //creating the user record in database
+      const user = await userModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: saltedPass,
+      });
+
+      //generating the jwt token based on user.id to send back to user
+      var token = jwt.sign({"id":user.id}, process.env.JWT_SIGN);
+
+      console.log(token);
+      res.json({'authToken': token});
+    } catch (e) {
+      console.log(e.message);
+      res.send(e.message);
+    }
   }
-  
-  userModel.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  }).then((user)=>{
-    console.log(user);
-    res.json(user);
-  }).catch((e)=>{
-    res.send(e.message);
-  });
-});
+);
 
 module.exports = router;
