b0794831b03d0dba41612f5fce44125dec2aef07
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-05-31 08:22:41 +0530

    CRUD of Notes Completed

--- routes/note.js
+++ routes/note.js
@@ -54,4 +54,67 @@ router.post(
   }
 );
 
+//ROUTE 3: Update Note
+router.put(
+  "/update:noteId",
+  auth,
+  [
+    body("title").isLength({ min: 3 }),
+    body("description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      //check if request body has proper title and description
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return res.send(errors);
+
+      //fetch the note from req.params.id given in request url
+      const noteId = req.params.noteId;
+      
+      //check if the note is of this user or he's tryna update someone else's note
+      let note = await noteModel.findOne({_id: noteId});
+      if(req.id.toString() !== note.userId.toString()) return res.send("Not your Note Nigga");
+
+      //if all above conditions are passed that means its safe to update the note
+      await noteModel.updateOne({ _id: noteId }, { 
+        title: req.body.title,
+        description: req.body.description,
+        tag: req.body.tag
+       });
+
+       note = await noteModel.findOne({_id: noteId});
+
+      console.log(note);
+      return res.send(note);
+    } catch (e) {
+      return res.send(e.message);
+    }
+  }
+);
+
+//ROUTE 4: Delete Note
+router.delete(
+  "/delete:noteId",
+  auth,
+  async (req, res) => {
+    try {
+      //fetch the note from req.params.id given in request url
+      const noteId = req.params.noteId;
+      
+      //check if the note is of this user or he's tryna update someone else's note
+      let note = await noteModel.findOne({_id: noteId});
+      if(req.id.toString() !== note.userId.toString()) return res.send("Not your Note Nigga");
+
+      //if all above conditions are passed that means its safe to delete the note
+      const dbAck = await noteModel.deleteOne({ _id: noteId });
+
+      console.log(dbAck);
+      return res.send(dbAck);
+    } catch (e) {
+      return res.send(e.message);
+    }
+  }
+);
+
+
 module.exports = router;
